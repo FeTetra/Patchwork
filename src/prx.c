a4e7778c0f7855ecb21691e4139aabbfe14cbf48
FeTetra <fetetra@protonmail.com>
2026-05-02 14:35:35 -0400

    Messy initial network downloading

--- src/prx.c
+++ src/prx.c
@@ -1,8 +1,13 @@
+#include <stdint.h>
+#include <string.h>
 #include <sys/prx.h>
 #include <sys/sys_time.h>
+#include <sys/memory.h>
 
 #include <cell/hash/libsha256.h>
 #include <cell/sysmodule.h>
+#include <cell/http.h>
+#include <netex/net.h>
 
 #include "hooks/hooks.h"
 #include "hooks/script-block.h"
@@ -15,13 +20,135 @@
 #include "toml/keymap.h"
 #include "toml/tokenizer.h"
 
+#define SIZE_1K (64 * 1024)
+#define URI_STRING "http://10.0.0.1:8000/patchwork.sprx"
+#define OUT_PATH "/dev_hdd0/plugins/patchwork/patchwork.sprx"
+
 SYS_MODULE_INFO(PatchworkLBP, 0, PATCHWORK_VERSION_MAJOR, PATCHWORK_VERSION_MINOR);
 SYS_MODULE_START(start);
 
 int start(void);
 int start(void)
 {
+    sys_addr_t *mem_page = (sys_addr_t *)0xFBEEF;
+    size_t top = 0;
+    sys_memory_allocate(SIZE_1K * 2, SYS_MEMORY_PAGE_SIZE_64K, mem_page);
+
+    println("loading modules\n");
     cellSysmoduleLoadModule(CELL_SYSMODULE_FS);
+    cellSysmoduleLoadModule(CELL_SYSMODULE_NET);
+    cellSysmoduleLoadModule(CELL_SYSMODULE_HTTP);
+
+    int ret;
+
+    println("initializing net\n");
+    ret = sys_net_initialize_network();
+    if (0 > ret) {
+        println("net wasnt initialized\n");
+    }
+
+    println("initializing http\n");
+    void *http_pool = mem_page + top;
+    size_t http_pool_size = SIZE_1K;
+    top += http_pool_size;
+    ret = cellHttpInit(http_pool, http_pool_size);
+    if (ret > 0) {
+        println("http wasnt initialized\n");
+    }
+
+    println("creating client\n");
+    CellHttpClientId client = 0;
+    ret = cellHttpCreateClient(&client);
+    if (0 > ret) {
+        println("client wasnt created\n");
+    }
+
+    println("parsing uri\n");
+    char uri_pool[64];
+    size_t expected_uri_size = 0;
+    CellHttpUri uri;
+    ret = cellHttpUtilParseUri(&uri, URI_STRING, uri_pool, 64, &expected_uri_size);
+    if (0 > ret) {
+        println("uri wasnt parsed\n");
+    }
+
+    println("creating transaction\n");
+    CellHttpTransId trans = 0;
+    ret = cellHttpCreateTransaction(&trans, client, CELL_HTTP_METHOD_GET, &uri);
+    if (0 > ret) {
+        println("transaction not created\n");
+    }
+
+    println("sending request\n");
+    ret = cellHttpSendRequest(trans, NULL, 0, NULL); // Why?
+    if (0 > ret) {
+        println("request not sent\n");
+    }
+
+    println("getting response length\n");
+    char numbuf[32];
+    memset(numbuf, 0, 32);
+    uint64_t length = 0;
+    ret = cellHttpResponseGetContentLength(trans, &length);
+    if (0 > ret) {
+        println("length was not gotten\n");
+    }
+    UIntToStr(numbuf, 32, length, 16);
+    println(numbuf);
+    println(": response size\n");
+
+    println("opening file to write\n");
+
+    int fp;
+    CellFsErrno err = cellFsOpen(OUT_PATH,
+        CELL_FS_O_WRONLY | CELL_FS_O_CREAT | CELL_FS_O_TRUNC,
+        &fp, NULL, 0);
+
+    if (err != CELL_FS_SUCCEEDED) {
+        println("file wasn't opened\n");
+    }
+
+    char buffer[256];
+    uint64_t recvd = 0;
+    size_t local_recv = 0;
+
+    println("receiving response\n");
+
+    while (1) {
+        int ret = cellHttpRecvResponse(trans, buffer, sizeof(buffer), &local_recv);
+
+        if (ret < 0) {
+            println("recv error\n");
+            break;
+        }
+
+        if (local_recv == 0) {
+            println("done\n");
+            break;
+        }
+
+        uint64_t offset = 0;
+
+        while (offset < local_recv) {
+            uint64_t written = 0;
+
+            cellFsWrite(fp,
+                buffer + offset,
+                local_recv - offset,
+                &written);
+
+            if (written == 0) {
+                println("write error\n");
+                break;
+            }
+
+            offset += written;
+        }
+
+        recvd += local_recv;
+    }
+
+    cellFsClose(fp);
 
     char toml_buf[256];
     ReadFile(MAIN_CONFIG_PATH, toml_buf, 256);
@@ -188,7 +315,11 @@ int start(void)
 
     // Exit
 
+    cellSysmoduleUnloadModule(CELL_SYSMODULE_HTTP);
+    cellSysmoduleUnloadModule(CELL_SYSMODULE_NET);
     cellSysmoduleUnloadModule(CELL_SYSMODULE_FS);
+
+    sys_memory_free(*mem_page);
 
     return SYS_PRX_NO_RESIDENT;
 }
